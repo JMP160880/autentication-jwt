45fe9d59ddbf736a95773d2d464c05b716dac5de
JMP160880 <javiermoranpando@gmail.com>
2023-02-23 20:16:51 +0000

    cierre 23-2

--- src/app.py
+++ src/app.py
@@ -192,6 +192,15 @@ def login():
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
 
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
 
 # Setup the Flask-JWT-Extended extension
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
