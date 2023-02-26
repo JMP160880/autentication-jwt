6e339314510c97f9dee397e108d0a5b67c425913
JMP160880 <javiermoranpando@gmail.com>
2023-02-26 12:11:39 +0000

    falta 2ª parte autenticacion

--- src/app.py
+++ src/app.py
@@ -250,17 +250,16 @@ def login():
 
 # Protect a route with jwt_required, which will kick out requests
 # without a valid JWT present.
+
+# ESTA PARTE ESTA PENDIENTE
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-jwt = JWTManager(app)
+    user = User.query.filter_by(email=current_user).first()
+    print(user)
+    return jsonify({"result":user.serialize()}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
